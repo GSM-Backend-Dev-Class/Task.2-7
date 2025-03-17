73b26fbad64f8a97c036d42a932dff6a52713a9b
KIM TAEEUN <snowykte0426@naver.com>
2025-03-18 00:42:08 +0900

    add application configuration for Spring Boot with MySQL and Redis

---
+++ src/main/java/com/gsm/_8th/class4/backend/task27/Task27Application.java
@@ -0,0 +1,13 @@
+package com.gsm._8th.class4.backend.task27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task27Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsm/_8th/class4/backend/task27/Task27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm._8th.class4.backend.task27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
